8432645f9012f872c27bc620b22b4bce2cf1057f
Matthias Beyer <mail@beyermatthias.de>
2014-07-06 01:11:54 +0200

    The expected attr count should be a Range, always

--- diary.rb
+++ diary.rb
@@ -673,7 +673,7 @@ module Diary
         instance = c.new()
 
         i = 0
-        until (instance.expected_attr_count || []).include?(i) || @argv.empty? do
+        while instance.expected_attr_count.include?(i) || (not @argv.empty?) do
           debug("Adding attribute to #{c} : #{@argv.first}")
           instance.add_attribute(@argv.pop)
           i += 1
