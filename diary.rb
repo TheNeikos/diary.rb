4dfde004561037f5906f9cc3bf59e35924b5b838
Matthias Beyer <mail@beyermatthias.de>
2014-07-05 17:24:32 +0200

    Everything should be in one module

--- diary.rb
+++ diary.rb
@@ -23,222 +23,226 @@ require 'find'
 require 'ostruct'
 require 'fileutils'
 
-class Config < Hash
+module Diary
 
-  # defaults
-  def initialize(other_config_path = false)
-    self[:root] = Dir.home + "/.diary"
-    self[:content_dir] = self[:root] + "/content"
-    self[:cfg] = other_config_path || self[:root] + "/diary.conf"
+  class Config < Hash
 
-    self[:ext] = "txt"
+    # defaults
+    def initialize(other_config_path = false)
+      self[:root] = Dir.home + "/.diary"
+      self[:content_dir] = self[:root] + "/content"
+      self[:cfg] = other_config_path || self[:root] + "/diary.conf"
 
-    self.merge non_default_config
-  end
+      self[:ext] = "txt"
 
-  def []=(k, v)
-    super[k.to_sym] = v
-  end
-
-  def [](k)
-    super[k.to_sym]
-  end
-
-  protected
-
-  def non_default_config
-    # TODO: read self[:cfg] file to hash and return
-  end
-
-end
-
-class Options < Hash
-  def []=(k, v)
-    k = non_arg k
-    key, value = parse(k, v)
-    super[key] = value
-  end
-
-  def [](k)
-    super[non_arg(k)]
-  end
-
-  protected
-
-  def non_arg(str)
-    str.gsub(/^--/, "")
-  end
-
-  def parse(key, value)
-    if key.include? "="
-      key = key.split("=").first
-      value = { key.split("=")[1] => value }
+      self.merge non_default_config
     end
 
-    [key, value]
-  end
-
-end
-
-module ConfigReader
-
-  def config=(c)
-    raise "Not a configuration: #{c.class} : #{c}" unless c.is_a? Config
-    @config = c
-  end
-
-end
-
-module CreateAbleFromPath
-
-  attr_reader :path
-
-  def self.from_path(path)
-    raise NoMethodException.new("Not implemented")
-  end
-
-  def self.subs_from_path(path, gen_class, &block)
-    Dir.new(path).entries.select(&block).map do |entry|
-      gen_class.from_path(path + "/" + entry)
+    def []=(k, v)
+      super[k.to_sym] = v
     end
-  end
 
-end
-
-module Indexable
-
-  attr_reader :index
-
-  def index_str(n = 2)
-    @index.to_s.rjust(n, "0")
-  end
-
-  def self.index_from_path(path, regex)
-    path.match(regex).to_s.to_i
-  end
-
-end
-
-module Iterateable
-
-  def each &block
-    raise NoMethodException.new("Not implemented")
-  end
-
-end
-
-class Entry
-  include ConfigReader
-  include CreateAbleFromPath
-  include Indexable
-
-  attr_accessor :time
-  attr_reader :content, :raw
-
-  def initialize(time, content)
-    @time = time
-    @content = content.encode(Encoding::UTF_8)
-    @raw = content
-  end
-
-  def self.from_path(path)
-    @time = self.time_from_path(path)
-    @raw = File.read path
-    @content = @raw.encode(Encoding::UTF_8)
-  end
-
-  protected
-
-  def self.time_from_path(path)
-    Time.parse path.match(/[0-2][0-9]-[0-9]{2,2}-[0-9]{2,2}/).to_s
-  end
-
-end
-
-class Day
-  include ConfigReader
-  include CreateAbleFromPath
-  include Indexable
-  include Iterateable
-
-  attr_accessor :entries
-
-  def initialize(entries, day_index = false)
-    @index = day_index || Date.today.day
-    @entries = entries
-  end
-
-  def each &block
-    @entries.each(&block)
-  end
-
-  def self.from_path(path)
-    @index = self.index_from_path(path, /[0-9]{2,2}$/)
-    @entries = self.subs_from_path(path, Entry, lambda { |e| File.file? e })
-  end
-
-end
-
-class Month
-  include ConfigReader
-  include CreateAbleFromPath
-  include Indexable
-  include Iterateable
-
-  attr_accessor :days
-
-  def initialize(days, month_index = false)
-    @index = month_index || Date.today.month
-    @days = days
-  end
-
-  def each &block
-    @days.each(&block)
-  end
-
-  def name
-    Date::MONTHNAMES[@index].downcase
-  end
-
-  def self.from_path(path)
-    @index = self.index_from_path(path, /[0-9]{2,2}$/)
-    @days = self.subs_from_path(path, Day, lambda { |e| File.directory? e })
-  end
-
-end
-
-class Year
-  include ConfigReader
-  include CreateAbleFromPath
-  include Iterateable
-
-  attr_accessor :months
-
-  def initialize(months, y = false)
-    @year = y || Date.today.year
-    @months = months
-  end
-
-  def each &block
-    @months.each(&block)
-  end
-
-  def self.from_path(path)
-    @path = path
-    @year = self.year_from_path path
-    @months = self.months_under path
-  end
-
-  protected
-
-  def self.year_from_path path
-    path.match(/[0-9]{4,4}$/).to_s.to_i
-  end
-
-  def self.months_undex path
-    Dir.new(path).entries.select { |sub| File.directory? sub }.map do |month|
-      Month.from_path(path + "/" + month)
+    def [](k)
+      super[k.to_sym]
     end
+
+    protected
+
+    def non_default_config
+      # TODO: read self[:cfg] file to hash and return
+    end
+
+  end
+
+  class Options < Hash
+    def []=(k, v)
+      k = non_arg k
+      key, value = parse(k, v)
+      super[key] = value
+    end
+
+    def [](k)
+      super[non_arg(k)]
+    end
+
+    protected
+
+    def non_arg(str)
+      str.gsub(/^--/, "")
+    end
+
+    def parse(key, value)
+      if key.include? "="
+        key = key.split("=").first
+        value = { key.split("=")[1] => value }
+      end
+
+      [key, value]
+    end
+
+  end
+
+  module ConfigReader
+
+    def config=(c)
+      raise "Not a configuration: #{c.class} : #{c}" unless c.is_a? Config
+      @config = c
+    end
+
+  end
+
+  module CreateAbleFromPath
+
+    attr_reader :path
+
+    def self.from_path(path)
+      raise NoMethodException.new("Not implemented")
+    end
+
+    def self.subs_from_path(path, gen_class, &block)
+      Dir.new(path).entries.select(&block).map do |entry|
+        gen_class.from_path(path + "/" + entry)
+      end
+    end
+
+  end
+
+  module Indexable
+
+    attr_reader :index
+
+    def index_str(n = 2)
+      @index.to_s.rjust(n, "0")
+    end
+
+    def self.index_from_path(path, regex)
+      path.match(regex).to_s.to_i
+    end
+
+  end
+
+  module Iterateable
+
+    def each &block
+      raise NoMethodException.new("Not implemented")
+    end
+
+  end
+
+  class Entry
+    include ConfigReader
+    include CreateAbleFromPath
+    include Indexable
+
+    attr_accessor :time
+    attr_reader :content, :raw
+
+    def initialize(time, content)
+      @time = time
+      @content = content.encode(Encoding::UTF_8)
+      @raw = content
+    end
+
+    def self.from_path(path)
+      @time = self.time_from_path(path)
+      @raw = File.read path
+      @content = @raw.encode(Encoding::UTF_8)
+    end
+
+    protected
+
+    def self.time_from_path(path)
+      Time.parse path.match(/[0-2][0-9]-[0-9]{2,2}-[0-9]{2,2}/).to_s
+    end
+
+  end
+
+  class Day
+    include ConfigReader
+    include CreateAbleFromPath
+    include Indexable
+    include Iterateable
+
+    attr_accessor :entries
+
+    def initialize(entries, day_index = false)
+      @index = day_index || Date.today.day
+      @entries = entries
+    end
+
+    def each &block
+      @entries.each(&block)
+    end
+
+    def self.from_path(path)
+      @index = self.index_from_path(path, /[0-9]{2,2}$/)
+      @entries = self.subs_from_path(path, Entry, lambda { |e| File.file? e })
+    end
+
+  end
+
+  class Month
+    include ConfigReader
+    include CreateAbleFromPath
+    include Indexable
+    include Iterateable
+
+    attr_accessor :days
+
+    def initialize(days, month_index = false)
+      @index = month_index || Date.today.month
+      @days = days
+    end
+
+    def each &block
+      @days.each(&block)
+    end
+
+    def name
+      Date::MONTHNAMES[@index].downcase
+    end
+
+    def self.from_path(path)
+      @index = self.index_from_path(path, /[0-9]{2,2}$/)
+      @days = self.subs_from_path(path, Day, lambda { |e| File.directory? e })
+    end
+
+  end
+
+  class Year
+    include ConfigReader
+    include CreateAbleFromPath
+    include Iterateable
+
+    attr_accessor :months
+
+    def initialize(months, y = false)
+      @year = y || Date.today.year
+      @months = months
+    end
+
+    def each &block
+      @months.each(&block)
+    end
+
+    def self.from_path(path)
+      @path = path
+      @year = self.year_from_path path
+      @months = self.months_under path
+    end
+
+    protected
+
+    def self.year_from_path path
+      path.match(/[0-9]{4,4}$/).to_s.to_i
+    end
+
+    def self.months_undex path
+      Dir.new(path).entries.select { |sub| File.directory? sub }.map do |month|
+        Month.from_path(path + "/" + month)
+      end
+    end
+
   end
 
 end
