9233a274fb6e9f573e26c3326cdd8405391f2817
Marcel Mueller <com2040@gmail.com>
2014-07-07 17:57:41 +0200

    Reverted errenous change

--- lib/diary.rb
+++ lib/diary.rb
@@ -38,3 +38,16 @@ require './tree_elements/year.rb'
 require './tree_elements/tree.rb'
 require './iterateable.rb'
 
+if __FILE__ == $0
+  config = {
+    :debug => true,
+    :root => "/tmp",
+    :content_dir => "/tmp/content",
+  }
+  cp = Diary::CommandParser::Parser.new(ARGV, config)
+  cp.parse!
+
+  ex = Diary::Executer.new(cp.commands, config)
+  ex.execute!
+end
+
